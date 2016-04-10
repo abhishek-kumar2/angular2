85409d3fae3af98a145b2549f67611efb2a65454
abhishek kumar <abhishek.kumar2@outlook.com>
2016-04-10 23:03:29 +0530

    Change me, event handler added

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,32 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+    <h1>{{name}}</h1>
+    <button (click)="onClickHandler()">{{button}}</button>
+    `
 })
-export class AppComponent { }
+export class AppComponent { 
+	name : string
+	button : string
+	
+	constructor(){
+		this.defaultStatus()
+	}
+
+	onClickHandler(){
+		if(this.button == "Change me"){
+			this.name = "Catch event and changed"
+			this.button = "Return"
+		}else{
+			this.defaultStatus()
+		}
+		
+	}
+
+	defaultStatus(){
+		this.name = "My First Angular 2 App"
+		this.button = "Change me"
+	}
+
+}
